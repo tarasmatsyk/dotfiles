52e761ec681324e67f9031bf4194327d16b444e2
Taras Matsyk <x@tar7k.com>
2025-04-06 11:40:07 +0200

    update fish config

--- shell/config.fish
+++ shell/config.fish
@@ -4,13 +4,14 @@ end
 
 # empty welcome message
 set fish_greeting
+set -Ux EDITOR nvim
 
-abbr gst 'git status'
-abbr gcd 'git checkout develop'
-abbr gcm 'git checkout master' 
-abbr gl  'git pull origin'
-abbr gca 'git commit --amend'
-abbr gpu 'git push -u origin'
+abbr	vim	'nvim'
+abbr	gst	'git status'
+abbr 	gcd	'git checkout develop'
+abbr 	gcm 	'git checkout master' 
+abbr 	gl	'git pull origin'
+abbr 	gca 	'git commit --amend'
+abbr 	gpu 	'git push -u origin'
 
-# pyenv config
-pyenv init - | source
+zoxide init fish | source
