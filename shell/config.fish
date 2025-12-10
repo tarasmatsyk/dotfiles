e42c32447f7ba257671619f9cf9dc80ca0bb2671
Taras Matsyk <x@tar7k.com>
2025-12-10 13:00:52 +0100

    add a few more abbr

--- shell/config.fish
+++ shell/config.fish
@@ -1,17 +1,20 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+else
 end
 
 # empty welcome message
-set fish_greeting
-set -Ux EDITOR nvim
+set	fish_greeting
 
-abbr	vim	'nvim'
-abbr	gst	'git status'
-abbr 	gcd	'git checkout develop'
-abbr 	gcm 	'git checkout master' 
-abbr 	gl	'git pull origin'
-abbr 	gca 	'git commit --amend'
-abbr 	gpu 	'git push -u origin'
+abbr	vim		'nvim'
+abbr	gst		'git status'
+abbr 	gcd		'git checkout develop'
+abbr 	gcm		'git checkout main' 
+abbr 	gcma	'git checkout master' 
+abbr 	gl		'git pull origin'
+abbr 	gca		'git commit --amend'
+abbr 	gpu		'git push -u origin'
+abbr 	k		'kubectl'
 
 zoxide init fish | source
+starship init fish | source
+mise activate fish | source
